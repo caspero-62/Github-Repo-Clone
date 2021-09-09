b7534be4ac54e12c332f11fad4d04d98c1e48a61
caspero-62 <nwaforuchechukwu2007@gmail.com>
2021-09-09 15:38:05 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import UserPage from './pages/userPage';
+import NotFound from './pages/notFound';
+import QueryPage from './pages/queryPage/queryPage';
+
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={QueryPage}/>
+          <Route exact path="/u/:userLogin" component={UserPage}/>
+          <Route component={NotFound}/>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/query/queryConfig.js
@@ -0,0 +1,75 @@
+export const queryData = (username) => `{
+    user(login: "${username.toString()}") {
+      avatarUrl
+      bio
+      email
+      followers {
+        totalCount
+      }
+      following {
+        totalCount
+      }
+      location
+      login
+      name
+      repositories(first: 20, orderBy: {field: UPDATED_AT, direction: DESC}) {
+        totalCount
+        nodes {
+          name
+          updatedAt
+          stargazerCount
+          forkCount
+          languages(first: 1, orderBy: {field: SIZE, direction: DESC}) {
+            nodes {
+              name
+              color
+            }
+          }
+          description
+          url
+          parent {
+            forkCount
+            name
+            licenseInfo {
+              name
+            }
+            languages(first: 1, orderBy: {field: SIZE, direction: DESC}) {
+              nodes {
+                color
+                name
+              }
+            }
+            isPrivate
+            nameWithOwner
+            url
+          }
+        }
+      }
+      twitterUsername
+      websiteUrl
+      status {
+        emojiHTML
+        emoji
+        message
+      }
+      starredRepositories {
+        totalCount
+      }
+      updatedAt
+    }
+  }`;
+  
+export const API_URL = 'https://api.github.com/graphql';
+
+export const token = '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';
+  
+export const queryOptions = (username) => ({
+  method: "POST",
+  headers: {
+    "Authorization": `bearer ${token}`,
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+    query: queryData(username)
+  }),
+});
